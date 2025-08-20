fdaa3c208f7ac8c921599823f9ff7b59ffa6038e
Sahil <sahil.22b1531101@abes.ac.in>
2025-08-20 20:15:26 +0530

    classification

--- Classification.py
+++ Classification.py
@@ -1,1 +1,25 @@
 #WAP to MNIST Digit Classification using Keras.
+
+import numpy as np
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Flatten
+from tensorflow.keras.utils import to_categorical
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train = x_train.astype("float32") / 255.0
+x_test = x_test.astype("float32") / 255.0
+y_train = to_categorical(y_train, 10)
+y_test = to_categorical(y_test, 10)
+model = Sequential([
+    Flatten(input_shape=(28, 28)),         
+    Dense(128, activation='relu'),       
+    Dense(64, activation='relu'),         
+    Dense(10, activation='softmax')        
+])
+model.compile(optimizer='adam',
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+model.fit(x_train, y_train, epochs=5, batch_size=32, validation_split=0.1)
+test_loss, test_acc = model.evaluate(x_test, y_test)
+print(f"\nTest accuracy: {test_acc:.4f}")
